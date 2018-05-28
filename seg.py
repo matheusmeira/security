8f045245372506a7ed215a8377677c58fed5d41e
Matheus Meira <meira.matheus.rob@gmail.com>
2018-05-28 20:37:02 -0300

    Adicionando método choices

--- seg.py
+++ seg.py
@@ -1,5 +1,6 @@
 from secrets import randbelow
 from hashlib import md5
+from hashlib import sha256
 from random import choices
 from collections import Counter
 
@@ -10,7 +11,6 @@ class BreakPassword:
     def __init__(self, password, probability):
         self.dictionary = self.readFile()
         self.passwordToBeBroken = password
-        # self.generateListOfProbability(probability)
         self.listOfProbability = probability
 
     def passwordBroken(self):
@@ -19,8 +19,6 @@ class BreakPassword:
             passwordHash = self.getPassword()
             self.passwordFound = passwordHash
             found = self.comparePassword(passwordHash)
-            # print(passwordHash)
-            # print(found)
         print(self.passwordFound)
 
     def comparePassword(self, passwordGenerate):
@@ -38,7 +36,7 @@ class BreakPassword:
             controll = self.onCheckPasswordInSet(password)
             if not controll:
                 self.setOfPassword.add(password)
-        return str(md5(password.encode('ascii')).hexdigest())
+        return str(sha256(password.encode('ascii')).hexdigest())
 
     def onCheckPasswordInSet(self, generatedPassword):
         return generatedPassword in self.setOfPassword
@@ -46,27 +44,8 @@ class BreakPassword:
     def selectNumber(self):
         s = ''
         numberForWord = choices(range(1, 7), weights = self.listOfProbability, k = 5)
-        # while(len(numberForWord) < 5):
-        #     numberRange = randbelow(100)
-        #     for i in range(6):
-        #         if numberRange < self.listOfProbability[i]:
-        #             numberForWord.append(str(i + 1))
-        #             break
         s = s.join(map(str, numberForWord))
-        # print(int(s))
         return int(s)
-
-    # def generateListOfProbability(self, listOfNumber):
-    #     for prob in listOfNumber:
-    #         if len(self.listOfProbability) == 0:
-    #             self.listOfProbability.append(round(prob * 100))
-    #         else:
-    #             if prob != 0:
-    #                 sum = self.listOfProbability[len(self.listOfProbability) - 1] + round(prob * 100)
-    #                 self.listOfProbability.append(sum)
-    #             else:
-    #                 self.listOfProbability.append(0)
-    #     print(self.listOfProbability)
             
     def readFile(self):
         dictionary = dict()
@@ -74,12 +53,11 @@ class BreakPassword:
             for line in f:
                 (key, value) = line.split()
                 dictionary[int(key)] = value
-                # print(key, value)
         return dictionary
 
 
 if __name__ == '__main__':
-    password = '2dcdb9fd95e7bf58056b47506dc3ebeb'
+    password = '5e816e8895fb2395ea89de9ada2ced74795d3df132f4b37e2ad7df268c553555'
     defaultProbabilities = [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.0, 0.0, 0.0]
     objBreakPassword = BreakPassword(password, defaultProbabilities)
     objBreakPassword.passwordBroken()
